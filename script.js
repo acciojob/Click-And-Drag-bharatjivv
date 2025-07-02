83d020178ce9bd3380f99606b92c059c6758d6e4
Bharat <hibharatj@gmail.com>
2025-07-02 13:43:04 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-cy.get('.items')
-  .trigger('mousedown', { which: 1, pageX: 493, pageY: 391, force: true })
-  .trigger('mousemove', { pageX: 271, pageY: 391, force: true })
-  .trigger('mouseup', { force: true });
-
-cy.get('.items').should($items => {
-  expect($items[0].scrollLeft).to.be.greaterThan(0);
-});
